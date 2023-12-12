f2f4e2556c6db9bfbf0289650b4d1d0b719f610a
Muhammad Umer <140950473+GitHubMuhammadUmer@users.noreply.github.com>
2023-12-12 08:08:49 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// var userName = prompt("Enter you name")
+// if(userName===''){
+//     alert("Input is empty")
+// }else{
+//     var email = prompt("Entet your email")
+//     if(email===""){
+//         alert("user name ")
+//     }
+// }
